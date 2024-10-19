af558d75d1329b6c79693aa682ae1b6d4750887d
Maksym Gorbachov <max.gorbachov@gmail.com>
2024-10-19 21:14:49 +0300

    reformat code

--- src/ContractedEmployee.java
+++ src/ContractedEmployee.java
@@ -1,36 +1,55 @@
-public class ContractedEmployee {
-    // TODO fix class declaration and declare variables here
+public class ContractedEmployee extends Employee {
+    @Description("Federal Tax ID for contracted employees")
+    private String federalTaxId;
+    private double hourlyRate;
+    private double numberOfHoursWorked;
 
     public ContractedEmployee(String employeeId, String name, String federalTaxId) {
-        // TODO fill in code here
+        super(employeeId, name);
+        this.federalTaxId = federalTaxId;
     }
 
     public String getFederalTaxId() {
-        // TODO fill in code here and replace the return statement
-        return "";
+        return this.federalTaxId;
     }
 
     public void setFederalTaxId(String federalTaxId) {
-        // TODO fill in code here
+        this.federalTaxId = federalTaxId;
     }
 
     public double getHourlyRate() {
-        // TODO fill in code here and replace the return statement
-        return 0;
+        return this.hourlyRate;
     }
 
     public void setHourlyRate(double hourlyRate) {
-        // TODO fill in code here
+        this.hourlyRate = hourlyRate;
     }
 
     public double getNumberOfHoursWorked() {
-        // TODO fill in code here and replace the return statement
-        return 0;
+        return this.numberOfHoursWorked;
     }
 
     public void setNumberOfHoursWorked(double numberOfHoursWorked) {
-        // TODO fill in code here
+        this.numberOfHoursWorked = numberOfHoursWorked;
     }
 
-    // TODO fill in code here
+    @Override
+    public double getAverageMonthlySalary() {
+        return hourlyRate * numberOfHoursWorked;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Employee ID: %s, Name: %s, Average Monthly Salary: %.2f",
+                getEmployeeId(), getName(), averageMonthlySalary);
+    }
+
+    @Override
+    public void calculatePay() {
+        averageMonthlySalary = hourlyRate * numberOfHoursWorked;
+    }
+}
+
+@interface Description {
+    String value();
 }
--- src/Employee.java
+++ src/Employee.java
@@ -1,36 +1,39 @@
-public class Employee {
+public abstract class Employee implements Payable{
     // TODO fix class declaration and declare variables here
+        String employeeId;
+        String name;
+        protected double averageMonthlySalary;
 
-    public Employee(String employeeId, String name) {
-        // TODO fill in code here
+        public Employee(String employeeId, String name) {
+            this.employeeId = employeeId;
+            this.name = name;
+
     }
 
     public String getEmployeeId() {
-        // TODO fill in code here and replace the return statement
-        return "";
+        return this.employeeId;
     }
 
     public void setEmployeeId(String employeeId) {
-        // TODO fill in code here
+        this.employeeId = employeeId;
     }
 
     public String getName() {
-        // TODO fill in code here and replace the return statement
-        return "";
+        return this.name;
     }
 
     public void setName(String name) {
-        // TODO fill in code here
+        this.name = name;
     }
 
+    @Override
     public double getAverageMonthlySalary() {
-        // TODO fill in code here and replace the return statement
-        return 0;
+        return averageMonthlySalary;
     }
 
     @Override
     public String toString() {
-        // TODO fill in code here and replace the return statement, be sure to format double value
-        return "";
+        return String.format("Employee ID: %s, Name: %s, Average Monthly Salary: %.2f", employeeId, name, averageMonthlySalary);
     }
+
 }
