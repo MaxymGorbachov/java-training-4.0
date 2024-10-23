48ccc5d17f7c1d8fa96eaf149f8790a1c41b98ca
Maksym Gorbachov <max.gorbachov@gmail.com>
2024-10-23 17:08:12 +0300

    resolve all todo add count methods add new test data

--- src/TemperatureTest.java
+++ src/TemperatureTest.java
@@ -14,8 +14,9 @@ public class TemperatureTest {
         return new Object[][]{
                 {212.0, 100.0},
                 {32.0, 0.0},
-                {-15, -26.11111111111111}
-                // TODO add 2 more test data here
+                {-15, -26.11111111111111},
+                {100.0,37.77777777777778},
+                {0,-17.77777777777778}
         };
     }
 
@@ -28,8 +29,10 @@ public class TemperatureTest {
         return new Object[][]{
                 {100.0, 212.0},
                 {0.0, 32.0},
-                {-26.11111111111111, -15}
-                // TODO add 2 more test data here
+                {-26.11111111111111, -15},
+                {37.0,98.6},
+                {-273,-459.4}
+
         };
     }
 
